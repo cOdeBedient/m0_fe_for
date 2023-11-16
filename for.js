20b1d880f595f75de1c019301c18eb99bb1600bd
Adam Bedient <adambedient@gmail.com>
2023-11-16 09:55:06 -0500

    Complete challenge 3

--- for.js
+++ for.js
@@ -19,6 +19,11 @@ for (i = 0; i < weatherOptions.length; i++){
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
 var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+for (i = 0; i < words.length; i++){
+  if (words[i].length === 3){
+  console.log(words[i]);
+  }
+}
 
 // 📹 Record yourself live-coding and talking through any ONE of the challenges. 
   // The recording doesn't have to be the first time you are solving the problem. 
